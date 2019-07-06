6ce2fbd1117a8328a82d7d7015d4cf253363dd33
Summer <su943515688@gmail.com>
2019-07-06 20:33:13 +0800

    [+] service 添加商品分类接口

--- common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public enum ExceptionEnum {
+    CATEGORY_NOT_FOUND(404, "商品分类未找到"),
     ;
     private int code;
     private String msg;
