f182bb723d66255248ea8f0e97caa4af89d632c4
Summer <su943515688@gmail.com>
2019-07-06 17:42:17 +0800

    [+] 添加common

---
+++ common/src/main/java/com/leyou/common/advice/CommonExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.leyou.common.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handler(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
